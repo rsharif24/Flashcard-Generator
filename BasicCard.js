5bb81f7d1b95a84dd48d705af991d62fd01c302c
Ruba <rsharif24@hotmail.com>
2017-07-30 15:52:23 -0500

    added constructor examples

--- BasicCard.js
+++ BasicCard.js
@@ -1,9 +1,12 @@
 
 module.exports = BasicCard;
 
-function BasicCard (front, back){
+function BasicCard(front, back) {
     this.front = front;
     this.back = back;
 };
 
+var president = new BasicCard("Who was the first president of the United States?", "George Washington");
 
+console.log(president.front);
+console.log(president.back);
--- ClozeCard.js
+++ ClozeCard.js
@@ -5,12 +5,15 @@ function ClozeCard(text, cloze) {
     this.cloze = cloze;
     this.partial = this.fullText.replace(this.cloze, "....");
 
-    if (!this.partial.includes(this.cloze)) {
+    if (!this.fullText.includes(this.cloze)) {
         throw ("Try Again");
 
     };
+
 };
 
-var test = new ClozeCard ("was first prez", "george wash")
 
-console.log(test.partial)
+var president = new ClozeCard("George Washington was the first president of the United States.", "George Washington");
+console.log(president.fullText);
+console.log(president.cloze);
+console.log(president.partial);
